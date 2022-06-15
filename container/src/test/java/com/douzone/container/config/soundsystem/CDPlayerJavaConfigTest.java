d047ec0c75ff5fdf79a34ae199d2b1cff9e513d2
김현석 <gustjr324@gmail.com>
2022-06-15 14:14:51 +0900

    Auto Configuration(Annotation Scanning)

--- container/src/test/java/com/douzone/container/config/soundsystem/CDPlayerJavaConfigTest.java
+++ container/src/test/java/com/douzone/container/config/soundsystem/CDPlayerJavaConfigTest.java
@@ -5,8 +5,7 @@ package com.douzone.container.config.soundsystem;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.Rule;
-import org.junit.contrib.java.lang.system.SystemOutRule;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +13,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.douzone.container.soundsystem.CDPlayer;
-import com.douzone.container.soundsystem.CompactDisc;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration("classpath:com/douzone/container/config/soundsystem/CDPlayerConfig.xml")
-public class CDPlayerXmlConfigTest {
-//	@Rule
-//	public final SystemOutRule systemOutRule = new SystemOutRule();
+@ContextConfiguration(classes={CDPlayerConfig.class})
+public class CDPlayerJavaConfigTest {
+
 	
 	@Autowired
 	CDPlayer cdPlayer;
